e21393c25c5d7726eaa30e9bb109a53796e013cb
ShiningStoneSmasher <klarlhauertstein@gmail.com>
2024-03-27 16:57:39 +0100

    first card animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
 
+  pickCardAnimation = false;
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+  takeCard(){
+    this.pickCardAnimation = true;
+  }
 }
