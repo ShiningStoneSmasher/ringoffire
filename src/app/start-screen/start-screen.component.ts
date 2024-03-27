a5fc75da8888b44be3733aaf8c599f321c9eaad2
ShiningStoneSmasher <klarlhauertstein@gmail.com>
2024-03-27 10:23:29 +0100

    first img and router

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './start-screen.component.html',
   styleUrls: ['./start-screen.component.css']
 })
 export class StartScreenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+
+  newGame(){
+    // start game
+    this.router.navigateByUrl('/game');
   }
 
 }
